6b28fd6c3bbedfeae2cb9e362c61a91c2d7055cc
kellel14 <kellel14@gmail.com>
2019-07-29 19:49:18 -0400

    501e

---
+++ src/main/java/com/example/demo/SpringBoot501eApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot501eApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot501eApplication.class, args);
+	}
+
+}
